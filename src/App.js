ae8dc6193cfd525881803683f855b29d858cc2e7
Taeho Choi <60216512+taeho-choi@users.noreply.github.com>
2022-04-13 20:13:40 +0900

    리액트 프로젝트 생성, 폰트 설정

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Main from "./Main";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div className="Main">
+      From "Hello, World!"
+      <br />
+      to Here
+    </div>
+  );
+};
+
+export default Main;
